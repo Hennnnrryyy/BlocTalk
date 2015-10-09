98bac7abddf149796a5f7613e8128717431d65f4
Henry <henrysmithjr@me.com>
2015-10-09 18:59:02 -0400

    User model classes created

---
+++ com.Hennnnrryyy.BlocTalk/com.Hennnnrryyy.BlocTalk/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  com.Hennnnrryyy.BlocTalk
+//
+//  Created by Henry Smith on 10/9/15.
+//  Copyright © 2015 Henry Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ com.Hennnnrryyy.BlocTalk/com.Hennnnrryyy.BlocTalk/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  com.Hennnnrryyy.BlocTalk
+//
+//  Created by Henry Smith on 10/9/15.
+//  Copyright © 2015 Henry Smith. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
